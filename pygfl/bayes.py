Fixed numerical stability issues. Added solution path for GTF with Gaussian loss

pygfl/bayes.py
@@ -28,7 +28,7 @@ gflbayes_gaussian_laplace.restype = None
 gflbayes_gaussian_laplace.argtypes = [c_int, ndpointer(c_double, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS'),
                 c_int, ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS'),
                 c_double, c_double,
-                c_int, c_int, c_int,
+                c_long, c_long, c_long,
                 ndpointer(dtype=np.uintp, ndim=1, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS')]
 
 gflbayes_gaussian_doublepareto = gflbayes_lib.bayes_gfl_gaussian_doublepareto
@@ -37,7 +37,7 @@ gflbayes_gaussian_doublepareto.argtypes = [c_int, ndpointer(c_double, flags='C_C
                 c_int, ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS'),
                 c_double, c_double,
                 c_double, c_double, c_double,
-                c_int, c_int, c_int,
+                c_long, c_long, c_long,
                 ndpointer(dtype=np.uintp, ndim=1, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS')]
 
 gflbayes_binomial_laplace = gflbayes_lib.bayes_gfl_binomial_laplace
@@ -45,7 +45,7 @@ gflbayes_binomial_laplace.restype = None
 gflbayes_binomial_laplace.argtypes = [c_int, ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_int, flags='C_CONTIGUOUS'),
                 c_int, ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS'),
                 c_double, c_double,
-                c_int, c_int, c_int,
+                c_long, c_long, c_long,
                 ndpointer(dtype=np.uintp, ndim=1, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS')]
 
 gflbayes_binomial_doublepareto = gflbayes_lib.bayes_gfl_binomial_doublepareto
@@ -54,7 +54,7 @@ gflbayes_binomial_doublepareto.argtypes = [c_int, ndpointer(c_int, flags='C_CONT
                 c_int, ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS'),
                 c_double, c_double,
                 c_double, c_double, c_double,
-                c_int, c_int, c_int,
+                c_long, c_long, c_long,
                 ndpointer(dtype=np.uintp, ndim=1, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS')]
 
 gflbayes_poisson_laplace = gflbayes_lib.bayes_gfl_poisson_laplace
@@ -62,7 +62,7 @@ gflbayes_poisson_laplace.restype = None
 gflbayes_poisson_laplace.argtypes = [c_int, ndpointer(c_int, flags='C_CONTIGUOUS'),
                 c_int, ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS'),
                 c_double, c_double,
-                c_int, c_int, c_int,
+                c_long, c_long, c_long,
                 ndpointer(dtype=np.uintp, ndim=1, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS')]
 
 gflbayes_poisson_doublepareto = gflbayes_lib.bayes_gfl_poisson_doublepareto
@@ -71,7 +71,7 @@ gflbayes_poisson_doublepareto.argtypes = [c_int, ndpointer(c_int, flags='C_CONTI
                 c_int, ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_int, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS'),
                 c_double, c_double,
                 c_double, c_double, c_double,
-                c_int, c_int, c_int,
+                c_long, c_long, c_long,
                 ndpointer(dtype=np.uintp, ndim=1, flags='C_CONTIGUOUS'), ndpointer(c_double, flags='C_CONTIGUOUS')]
 
 trunc_norm = gflbayes_lib.rnorm_trunc_norand
@@ -102,7 +102,7 @@ def sample_gtf(data, D, k, likelihood='gaussian', prior='laplace',
 
     if prior == 'laplace':
         if lambda_hyperparams == None:
-            lambda_hyperparams = (0.5, 0.5)
+            lambda_hyperparams = (0.5, np.sqrt(D.shape[0]))
     elif prior == 'doublepareto':
         if lambda_hyperparams == None:
             lambda_hyperparams = (0.01, 0.01)
@@ -111,8 +111,8 @@ def sample_gtf(data, D, k, likelihood='gaussian', prior='laplace',
 
     # Run the Gibbs sampler
     sample_size = (iterations - burn) / thin
-    beta_samples = np.zeros((sample_size, D.shape[1]))
-    lam_samples = np.zeros(sample_size)
+    beta_samples = np.zeros((sample_size, D.shape[1]), dtype='double')
+    lam_samples = np.zeros(sample_size, dtype='double')
 
     if likelihood == 'gaussian':
         if prior == 'laplace':
@@ -267,7 +267,7 @@ def test_sample_gtf_gaussian():
     
     D = get_1d_penalty_matrix(len(y))
 
-    z_samples, lam_samples = sample_gtf((y, w), D, k, likelihood='gaussian', prior='doublepareto', verbose=True)
+    z_samples, lam_samples = sample_gtf((y, w), D, k, likelihood='gaussian', prior='laplace', verbose=True)
     y *= stdev_offset
     y += mean_offset
     z_samples *= stdev_offset

pygfl/trendfiltering.py
@@ -18,7 +18,8 @@
 import matplotlib.pylab as plt
 import numpy as np
 from numpy.ctypeslib import ndpointer
-from scipy.sparse import coo_matrix
+from scipy.sparse import coo_matrix, csr_matrix
+from collections import defaultdict
 from ctypes import *
 from utils import *
 
@@ -62,6 +63,7 @@ class TrendFilteringSolver:
         self.steps = []
         self.Dk = None
         self.u = None
+        self.edges = None
 
     def solve(self, k, lam):
         '''Solves the GFL for a fixed value of lambda.'''
@@ -78,6 +80,84 @@ class TrendFilteringSolver:
                              self.beta, self.u)
         self.steps.append(s)
         return self.beta
+
+    def solution_path(self, k, min_lambda, max_lambda, lambda_bins, verbose=0):
+        '''Follows the solution path to find the best lambda value.'''
+        self.Dk = get_delta(self.D, k).tocoo()
+        self.u = np.zeros(self.Dk.shape[0], dtype='double')
+        lambda_grid = np.exp(np.linspace(np.log(max_lambda), np.log(min_lambda), lambda_bins))
+        aic_trace = np.zeros(lambda_grid.shape) # The AIC score for each lambda value
+        aicc_trace = np.zeros(lambda_grid.shape) # The AICc score for each lambda value (correcting for finite sample size)
+        bic_trace = np.zeros(lambda_grid.shape) # The BIC score for each lambda value
+        dof_trace = np.zeros(lambda_grid.shape) # The degrees of freedom of each final solution
+        log_likelihood_trace = np.zeros(lambda_grid.shape)
+        beta_trace = []
+        best_idx = None
+        best_plateaus = None
+        Dk_minus_one = get_delta(self.D, k-1) if k > 0 else None
+
+        if self.edges is None:
+            self.edges = defaultdict(list)
+            elist = csr_matrix(self.D).indices.reshape((self.D.shape[0], 2))
+            for n1, n2 in elist:
+                self.edges[n1].append(n2)
+                self.edges[n2].append(n1)
+
+        # Solve the series of lambda values with warm starts at each point
+        for i, lam in enumerate(lambda_grid):
+            if verbose:
+                print '#{0} Lambda = {1}'.format(i, lam)
+
+            # Fit to the final values
+            beta = self.solve(k, lam)
+
+            if verbose:
+                print 'Calculating degrees of freedom'
+
+            # Count the number of free parameters in the grid (dof) -- TODO: the graph trend filtering paper seems to imply we shouldn't multiply by (k+1)?
+            dof_vals = Dk_minus_one.dot(beta) if k > 0 else beta
+            plateaus = calc_plateaus(dof_vals, self.edges) if (k % 2) == 0 else nearly_unique(dof_vals)
+            dof_trace[i] = max(1,len(plateaus)) #* (k+1)
+
+            if verbose:
+                print 'Calculating Information Criteria'
+
+            # Get the negative log-likelihood
+            log_likelihood_trace[i] = -0.5 * ((self.y - beta)**2).sum()
+
+            # Calculate AIC = 2k - 2ln(L)
+            aic_trace[i] = 2. * dof_trace[i] - 2. * log_likelihood_trace[i]
+            
+            # Calculate AICc = AIC + 2k * (k+1) / (n - k - 1)
+            aicc_trace[i] = aic_trace[i] + 2 * dof_trace[i] * (dof_trace[i]+1) / (len(beta) - dof_trace[i] - 1.)
+
+            # Calculate BIC = -2ln(L) + k * (ln(n) - ln(2pi))
+            bic_trace[i] = -2 * log_likelihood_trace[i] + dof_trace[i] * (np.log(len(beta)) - np.log(2 * np.pi))
+
+            # Track the best model thus far
+            if best_idx is None or bic_trace[i] < bic_trace[best_idx]:
+                best_idx = i
+                best_plateaus = plateaus
+
+            # Save the trace of all the resulting parameters
+            beta_trace.append(np.array(beta))
+            
+            if verbose:
+                print 'DoF: {0} AIC: {1} AICc: {2} BIC: {3}'.format(dof_trace[i], aic_trace[i], aicc_trace[i], bic_trace[i])
+
+        if verbose:
+            print 'Best setting (by BIC): lambda={0} [DoF: {1}, AIC: {2}, AICc: {3} BIC: {4}]'.format(lambda_grid[best_idx], dof_trace[best_idx], aic_trace[best_idx], aicc_trace[best_idx], bic_trace[best_idx])
+
+        return {'aic': aic_trace,
+                'aicc': aicc_trace,
+                'bic': bic_trace,
+                'dof': dof_trace,
+                'loglikelihood': log_likelihood_trace,
+                'beta': np.array(beta_trace),
+                'lambda': lambda_grid,
+                'best_idx': best_idx,
+                'best': beta_trace[best_idx],
+                'plateaus': best_plateaus}
 
 class LogitTrendFilteringSolver:
     def __init__(self, maxsteps=3000, converge=1e-6):
@@ -153,7 +233,7 @@ def test_solve_gtf():
     # equally weight each data point
     w = np.ones(len(y))
 
-    lam = 3.
+    lam = 50.
 
     # try different weights for each data point
     # w = np.ones(len(y))
@@ -163,10 +243,11 @@ def test_solve_gtf():
     D = coo_matrix(get_1d_penalty_matrix(len(y)))
 
     z = np.zeros((max_k,len(y)))
+    tf = TrendFilteringSolver()
+    tf.set_data(y, D, w)
     for k in xrange(max_k):
-        tf = TrendFilteringSolver()
-        tf.set_data(y, D, w)
-        z[k] = tf.solve(k, lam)
+        #z[k] = tf.solve(k, lam)
+        z[k] = tf.solution_path(k, 0.2, 2000, 100, verbose=True)['best']
     
     y *= stdev_offset
     y += mean_offset
@@ -258,4 +339,4 @@ def test_solve_gtf_poisson():
     plt.clf()
 
 if __name__ == '__main__':
-    test_solve_gtf_poisson()
+    test_solve_gtf()

pygfl/utils.py
@@ -101,22 +101,22 @@ def sparse_matrix_to_edges(data):
             edges[y].append(x)
     return edges
 
-def pretty_str(p, decimal_places=2):
+def pretty_str(p, decimal_places=2, print_zero=True, label_columns=False):
     '''Pretty-print a matrix or vector.'''
     if len(p.shape) == 1:
-        return vector_str(p, decimal_places)
+        return vector_str(p, decimal_places, print_zero)
     if len(p.shape) == 2:
-        return matrix_str(p, decimal_places)
+        return matrix_str(p, decimal_places, print_zero, label_columns)
     raise Exception('Invalid array with shape {0}'.format(p.shape))
 
-def matrix_str(p, decimal_places=2):
+def matrix_str(p, decimal_places=2, print_zero=True, label_columns=False):
     '''Pretty-print the matrix.'''
-    return '[{0}]'.format("\n  ".join([vector_str(a, decimal_places) for a in p]))
+    return '[{0}]'.format("\n  ".join([(str(i) if label_columns else '') + vector_str(a, decimal_places, print_zero) for i, a in enumerate(p)]))
 
-def vector_str(p, decimal_places=2):
+def vector_str(p, decimal_places=2, print_zero=True):
     '''Pretty-print the vector values.'''
     style = '{0:.' + str(decimal_places) + 'f}'
-    return '[{0}]'.format(", ".join([style.format(a) for a in p]))
+    return '[{0}]'.format(", ".join([' ' if not print_zero and a == 0 else style.format(a) for a in p]))
 
 
 def make_directory(base, subdir):
@@ -196,6 +196,14 @@ def calc_plateaus(beta, edges, rel_tol=1e-4, verbose=0):
     # Returns the list of plateaus and their values
     return plateaus
 
+def nearly_unique(arr, rel_tol=1e-4, verbose=0):
+    '''Heuristic method to return the uniques within some precision in a numpy array'''
+    results = np.array([arr[0]])
+    for x in arr:
+        if np.abs(results - x).min() > rel_tol:
+            results = np.append(results, x)
+    return results
+
 def grid_graph_edges(rows, cols):
     edges = []
     for x in xrange(cols):
@@ -231,10 +239,72 @@ def row_col_trails(rows, cols):
     breakpoints[rows:] = nnodes + np.arange(1, cols+1) * rows
     return ntrails, trails, breakpoints, grid_graph_edges(rows, cols)
 
-def get_1d_penalty_matrix(length):
-    D = np.eye(length, dtype=float)[0:-1] * -1
-    for i in xrange(len(D)):
-        D[i,i+1] = 1
+def get_1d_penalty_matrix(length, sparse=False):
+    if sparse:
+        rows = np.repeat(np.arange(length-1), 2)
+        cols = np.repeat(np.arange(length), 2)[1:-1]
+        data = np.tile([-1, 1], length-1)
+        D = coo_matrix((data, (rows, cols)), shape=(length-1, length))
+    else:
+        D = np.eye(length, dtype=float)[0:-1] * -1
+        for i in xrange(len(D)):
+            D[i,i+1] = 1
+    return D
+
+def get_2d_penalty_matrix(rows, cols, sparse=True):
+    r = 0
+    rvals = []
+    cvals = []
+    data = []
+    for y in xrange(rows):
+        for x in xrange(cols - 1):
+            rvals.append(r)
+            rvals.append(r)
+            r += 1
+            cvals.append(y*cols+x)
+            cvals.append(y*cols+x+1)
+            data.append(-1)
+            data.append(1)
+    for y in xrange(rows - 1):
+        for x in xrange(cols):
+            rvals.append(r)
+            rvals.append(r)
+            r += 1
+            cvals.append(y*cols+x)
+            cvals.append((y+1)*cols+x)
+            data.append(-1)
+            data.append(1)
+    D = coo_matrix((data, (rvals, cvals)), shape=(r, rows*cols))
+    if not sparse:
+        D = np.array(D.todense())
+    return D
+
+def special_2d(rows, cols, sparse=True):
+    r = 0
+    rvals = []
+    cvals = []
+    data = []
+    for y in xrange(rows - 1):
+        for x in xrange(cols):
+            rvals.append(r)
+            rvals.append(r)
+            r += 1
+            cvals.append(y*cols+x)
+            cvals.append((y+1)*cols+x)
+            data.append(-1)
+            data.append(1)
+    for y in xrange(cols - 1):
+        for x in xrange(rows):
+            rvals.append(r)
+            rvals.append(r)
+            r += 1
+            cvals.append(y*rows+x+rows*cols)
+            cvals.append((y+1)*rows+x+rows*cols)
+            data.append(1)
+            data.append(-1)
+    D = coo_matrix((data, (rvals, cvals)), shape=(r, rows*cols*2))
+    if not sparse:
+        D = np.array(D.todense())
     return D
 
 def get_delta(D, k):
@@ -254,8 +324,8 @@ def decompose_delta(deltak):
         deltak = coo_matrix(deltak)
     dk_rows = deltak.shape[0]
     dk_rowbreaks = np.cumsum(deltak.getnnz(1), dtype="int32")
-    dk_cols = deltak.col
-    dk_vals = deltak.data
+    dk_cols = deltak.col.astype('int32')
+    dk_vals = deltak.data.astype('double')
     return dk_rows, dk_rowbreaks, dk_cols, dk_vals
 
 